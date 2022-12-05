dbdeb57b3e4371baeebc9b7fae0640126b3e9b5f
Quartz68 <wangzimeng2004@gmail.com>
2022-12-05 23:07:08 +0800

    Added comment to food and table

--- libs/restaurant_util/include/food.hpp
+++ libs/restaurant_util/include/food.hpp
@@ -6,20 +6,21 @@
 using namespace std;
 
 struct FoodID {
-    unsigned short type : 16;
-    unsigned short id : 16;
+    unsigned short type : 16; // 菜品类型id
+    unsigned short id : 16; // 菜品在这个类型的id，两个接起来就是这个菜品独有的id
 };
 
 class Price {
 public:
     Price(double price)
         :abs_price_(price) { }
-    double get_cny();
-    double get_usd();
-    void set_price(double price);
-    void set_discount(double percentage);
+    double get_cny(); // 返回人民币价格
+    double get_usd(); // 返回美元价格
+    void set_price(double price); // 设置价格
+    void set_discount(double percentage); // 设置打折
 private:
-    double abs_price_; // in CNY
+    double abs_price_; // 人民币绝对值价格
+    double discount_; // 折扣
 };
 
 struct FoodType {
@@ -31,15 +32,15 @@ struct FoodType {
 
 class Food {
 public:
-    Food(string name, double price, unsigned short type_id, unsigned short id);
-    string get_name();
-    double get_price_cny();
-    double get_price_usd();
-    string get_type_name();
-    string get_id_str();
-    void set_name(string name);
-    void set_price(double price);
-    void set_type(unsigned short type_id);
+    Food(string name, double price, unsigned short type_id, unsigned short id); //菜品构造函数
+    string get_name(); // 返回菜品名
+    double get_price_cny(); // 返回人民币价格
+    double get_price_usd(); // 返回美元价格
+    string get_type_name(); // 返回菜品类型名
+    string get_id_str(); // 返回菜品ID字符串
+    void set_name(string name); //设置菜品名称
+    void set_price(double price); //设置菜品价格
+    void set_type(unsigned short type_id);//设置菜品类型
 private:
     string name_;
     FoodType& type_;
--- libs/restaurant_util/include/table.hpp
+++ libs/restaurant_util/include/table.hpp
@@ -5,26 +5,27 @@
 #include <unordered_map>
 using namespace std;
 
-struct TableID {
-    unsigned short type : 16;
-    unsigned short id : 16;
+struct TableID { //桌子的id
+    unsigned short type : 16; //桌子类型的id，也就是TableType::id
+    unsigned short id : 16; //桌子自己在这个类型的id，两个接起来就是这个桌子独有的id
 };
 
+//桌子类型
 struct TableType {
     TableType(string name, unsigned short capacity, unsigned short id)
         :name(name), capacity(capacity), id(id) { }
-    string name;
-    unsigned short capacity;
-    unsigned short id;
+    string name;//桌子类型名
+    unsigned short capacity;//桌子能坐人数
+    unsigned short id;//桌子类型的id
 };
 
 class Table {
 public:
-    Table(unsigned short type_id, unsigned short id);
-    string get_type_name();
-    string get_id_str();
-    unsigned short get_capacity();
-    void set_type(unsigned short type_id);
+    Table(unsigned short type_id, unsigned short id); //构造函数
+    string get_type_name(); //返回桌子类型名
+    string get_id_str(); //返回桌子id字符串
+    unsigned short get_capacity(); // 返回桌子能坐的人数
+    void set_type(unsigned short type_id); // 设置桌子类型
 private:
     TableType& type_;
     TableID table_id_;
